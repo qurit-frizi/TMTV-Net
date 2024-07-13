250988cfab954b43fb5f7f8e66498720e3d4f155
Fereshteh Yousefirizi <84542058+qurit-frizi@users.noreply.github.com>
2024-07-12 18:23:23 -0700

    Update DicomNiftiConversion.py

--- main/src/DicomNiftiConversion.py
+++ main/src/DicomNiftiConversion.py
@@ -39,8 +39,8 @@ def bqml_to_suv(dcm_file: pydicom.FileDataset) -> float:
 
     parse = lambda x: dateutil.parser.parse(x)
 
-    series_time = str(dcm_file[0x0008, 0x0031].value)  # Series start time (hh:mm:ss)
-    series_date = str(dcm_file[0x0008, 0x0021].value)  # Series start date (yyy:mm:dd)
+    series_time = str(dcm_file[0x0008, 0x00031].value)  # Series start time (hh:mm:ss)
+    series_date = str(dcm_file[0x0008, 0x00021].value)  # Series start date (yyy:mm:dd)
     series_datetime_str = series_date + ' ' + series_time
     series_dt = parse(series_datetime_str)
 
@@ -49,10 +49,12 @@ def bqml_to_suv(dcm_file: pydicom.FileDataset) -> float:
     nuclide_dt = parse(nuclide_datetime_str)
 
     delta_time = (series_dt - nuclide_dt).total_seconds()
-    decay_correction = 2 ** (-1 * delta_time / half_life)
+    decay_correction = 2 ** (-1 * delta_time/half_life)
     suv_factor = (weight * 1000) / (decay_correction * nuclide_dose)
+    Rescale_Slope= dcm_file[0x0028,0x1053].value
+    Rescale_Intercept=dcm_file[0x0028,0x1052].value
 
-    return suv_factor
+    return (suv_factor, Rescale_Slope, Rescale_Intercept)
 
 
 def list_of_modality_dirs(input_dir):
@@ -68,18 +70,17 @@ def list_of_modality_dirs(input_dir):
                 Patient_ID = ds.PatientID
                 if ds.Modality == 'PT':
                     print('Found PET DIR')
-                    pt_dir = os.path.join(input_dir, direc)
+                    pt_dir = os.path.join(input_dir,direc)
                     break
                 elif ds.Modality == 'CT':
                     print('Found CT DIR')
-                    ct_dir = os.path.join(input_dir, direc)
+                    ct_dir = os.path.join(input_dir,direc)
                     break
                 else:
                     print('Found dir without PET or CT')
                     break
-    modality_dirs = {'PT': pt_dir, 'CT': ct_dir, 'ID': Patient_ID}
+    modality_dirs = {'PT':pt_dir, 'CT':ct_dir, 'ID': Patient_ID}
     return modality_dirs
-
 
 def read_slices_from_dir(input_dir):
     # read and sort .dcm slices from a directory
@@ -100,55 +101,63 @@ def read_slices_from_dir(input_dir):
             slices.append(f)
         else:
             skipcount += 1
-    # print('Skipped {} files'.format(skipcount))
-    slices = sorted(slices, key=lambda s: s.SliceLocation)
+    #print('Skipped {} files'.format(skipcount))
+    slices = sorted(slices,key=lambda s: s.SliceLocation)
     return slices
-
 
 def dicomToNifti(seriesDir, savePath):
     # converts DICOM series in the seriesDir to NIFTI image in the savePath specified
-    dicom_files = read_slices_from_dir(seriesDir)
-
-    if not dicom_files:
-        raise RuntimeError("No DICOM files found in the specified directory.")
+    for root, dirs, files in os.walk(seriesDir):
+        for file in files:
+            if file.endswith('.dcm'):
+                filename = os.path.join(root, file)
+                ds = pydicom.dcmread(filename, force=True)
+                break
 
     traits = {
-        "Patient ID": getattr(dicom_files[0], 'PatientID', None),
-        "Patient's Sex": getattr(dicom_files[0], 'PatientSex', None),
-        "Patient's Age": getattr(dicom_files[0], 'PatientAge', None),
-        "Patient's Birth Date": getattr(dicom_files[0], 'PatientBirthDate', None),
-        "Patient's Weight": getattr(dicom_files[0], 'PatientWeight', None),
-        "Institution Name": getattr(dicom_files[0], 'InstitutionName', None),
-        "Referring Physician's Name": getattr(dicom_files[0], 'ReferringPhysicianName', None),
-        "Operator's Name": getattr(dicom_files[0], 'OperatorsName', None),
-        "Study Date": getattr(dicom_files[0], 'StudyDate', None),
-        "Study Time": getattr(dicom_files[0], 'StudyTime', None),
-        "Modality": getattr(dicom_files[0], 'Modality', None),
-        "Series Description": getattr(dicom_files[0], 'SeriesDescription', None),
-        "Dimensions": np.array(getattr(dicom_files[0], 'pixel_array', None)).shape,
-    }
+            "Patient ID":
+            getattr(ds, 'PatientID', None),
+            "Patient's Sex":
+            getattr(ds, 'PatientSex', None),
+            "Patient's Age":
+            getattr(ds, 'PatientAge', None),
+            "Patient's Birth Date":
+            getattr(ds, 'PatientBirthDate', None),
+            "Patient's Weight":
+            getattr(ds, 'PatientWeight', None),
+            "Institution Name":
+            getattr(ds, 'InstitutionName', None),
+            "Referring Physician's Name":
+            getattr(ds, 'ReferringPhysicianName', None),
+            "Operator's Name":
+            getattr(ds, 'OperatorsName', None),
+            "Study Date":
+            getattr(ds, 'StudyDate', None),
+            "Study Time":
+            getattr(ds, 'StudyTime', None),
+            "Modality":
+            getattr(ds, 'Modality', None),
+            "Series Description":
+            getattr(ds, 'SeriesDescription', None),
+            "Dimensions":
+            np.array(getattr(ds, 'pixel_array', None)).shape,
+        }
 
     reader = sitk.ImageSeriesReader()
-    seriesNames = [ds.filename for ds in dicom_files]
+    seriesNames = reader.GetGDCMSeriesFileNames(seriesDir)
     reader.SetFileNames(seriesNames)
     image = reader.Execute()
 
     if traits["Modality"] == 'PT':
-        suv_factor = bqml_to_suv(dicom_files[0])  # Calculate SUV factor using the first DICOM file
+        pet = pydicom.dcmread(seriesNames[0])  # read one of the images for header info
+        suv_result = bqml_to_suv(pet)
+        suv_factor = suv_result[0]
+        Rescale_Slope = 1; #suv_result[1]
+        Rescale_Intercept = 0; #suv_result[2]
 
-        # Apply Rescale Slope and Intercept for each slice
-        for slice_file in dicom_files:
-            rescale_slope = slice_file[0x0028, 0x1053].value
-            rescale_intercept = slice_file[0x0028, 0x1052].value
-
-            slice_index = dicom_files.index(slice_file)
-            image_slice = sitk.VectorIndexSelectionCast(image, slice_index)
-
-            image_slice = sitk.Multiply(image_slice, rescale_slope)
-            image_slice = sitk.Add(image_slice, rescale_intercept)
-            image_slice = sitk.Multiply(image_slice, suv_factor)
-
-            image = sitk.Paste(image, image_slice, image_slice.GetSize(), image_slice.GetIndex(), image_slice.GetIndex())
+        image = sitk.Multiply(image, Rescale_Slope)
+        image = image + Rescale_Intercept
+        image = sitk.Multiply(image, suv_factor)
 
     sitk.WriteImage(image, os.path.join(savePath, f'{traits["Patient ID"]}_{traits["Modality"]}_{traits["Study Date"]}.nii.gz'), imageIO='NiftiImageIO')
     return os.path.join(savePath, f'{traits["Patient ID"]}_{traits["Modality"]}_{traits["Study Date"]}.nii.gz')
